04c0975e5149577c211250cee7eccdca869e3928
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-10-26 00:31:15 +0200

    Rewriten preferences for simplifying things in other clases.

--- src/es/pulimento/wifi/Preferences.java
+++ src/es/pulimento/wifi/Preferences.java
@@ -1,98 +1,73 @@
 package es.pulimento.wifi;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 
-public class Preferences extends PreferenceActivity {
-	
-//	private SharedPreferences s;
-//	private CheckBoxPreference au;// auto-update list in SCAN Mode
-//	private CheckBoxPreference vF;// vibrate when found a vulnerable network
-//	private CheckBoxPreference vU;// vibrate on each autoupdate of the list (@requires au==true)
-//	private ListPreference uI;//Update interval (2,5,10 seconds)
-	
+public class Preferences extends PreferenceActivity implements OnPreferenceChangeListener {
+
+	/* Constants. */
+	private final String PREFERENCES_NAME = "preferences";
+
+	/* Needed variables. */
+	private Context mContext;
+	private SharedPreferences mPreferences;
+
+	/* Preference keys. */
+	public final String PREFERENCES_AUTOUPDATE_KEY;
+	public final String PREFERENCES_VIBRATEUPDATE_KEY;
+	public final String PREFERENCES_VIBRATEFOUND_KEY;
+	public final String PREFERENCES_UPDATEINTERVAL_KEY;
+
+	/* Preference default values. */
+	public final boolean PREFERENCES_AUTOUPDATE_DEFAULT;
+	public final boolean PREFERENCES_VIBRATEUPDATE_DEFAULT;
+	public final boolean PREFERENCES_VIBRATEFOUND_DEFAULT;
+	public final int PREFERENCES_UPDATEINTERVAL_DEFAULT;
+
+	/* Current preferences. */
+	public boolean PREFERENCES_AUTOUPDATE_CURRENT;
+	public boolean PREFERENCES_VIBRATEUPDATE_CURRENT;
+	public boolean PREFERENCES_VIBRATEFOUND_CURRENT;
+	public int PREFERENCES_UPDATEINTERVAL_CURRENT;
+
+	/* Class constructor for getting preferences. */
+	Preferences(Context ctx) {
+		mContext = ctx;
+		mPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+		PREFERENCES_AUTOUPDATE_KEY = mContext.getString(R.string.preferences_autoupdate_key);
+		PREFERENCES_AUTOUPDATE_DEFAULT = Boolean.parseBoolean(mContext.getString(R.string.preferences_autoupdate_default));
+		PREFERENCES_VIBRATEUPDATE_KEY = mContext.getString(R.string.preferences_vibrateupdate_key);
+		PREFERENCES_VIBRATEUPDATE_DEFAULT = Boolean.parseBoolean(mContext.getString(R.string.preferences_vibrateupdate_default));
+		PREFERENCES_VIBRATEFOUND_KEY = mContext.getString(R.string.preferences_vibratefound_key);
+		PREFERENCES_VIBRATEFOUND_DEFAULT = Boolean.parseBoolean(mContext.getString(R.string.preferences_vibratefound_default));
+		PREFERENCES_UPDATEINTERVAL_KEY = mContext.getString(R.string.preferences_updateinterval_key);
+		PREFERENCES_UPDATEINTERVAL_DEFAULT = Integer.parseInt(mContext.getString(R.string.preferences_vibratefound_default));
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+
+		PreferenceScreen prefScreen = this.getPreferenceScreen();
+		prefScreen.findPreference(PREFERENCES_AUTOUPDATE_KEY).setOnPreferenceChangeListener(this);
+		prefScreen.findPreference(PREFERENCES_UPDATEINTERVAL_KEY).setOnPreferenceChangeListener(this);
+		prefScreen.findPreference(PREFERENCES_VIBRATEFOUND_KEY).setOnPreferenceChangeListener(this);
+		prefScreen.findPreference(PREFERENCES_VIBRATEUPDATE_KEY).setOnPreferenceChangeListener(this);
 	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		PREFERENCES_AUTOUPDATE_CURRENT = mPreferences.getBoolean(PREFERENCES_AUTOUPDATE_KEY, PREFERENCES_AUTOUPDATE_DEFAULT);
+		PREFERENCES_VIBRATEUPDATE_CURRENT = mPreferences.getBoolean(PREFERENCES_VIBRATEUPDATE_KEY, PREFERENCES_VIBRATEUPDATE_DEFAULT);
+		PREFERENCES_VIBRATEFOUND_CURRENT = mPreferences.getBoolean(PREFERENCES_VIBRATEFOUND_KEY, PREFERENCES_VIBRATEFOUND_DEFAULT);
+		PREFERENCES_UPDATEINTERVAL_CURRENT = Integer.parseInt(mPreferences.getString(PREFERENCES_UPDATEINTERVAL_KEY, String.valueOf(PREFERENCES_UPDATEINTERVAL_DEFAULT)));
+		return true;
+    }
+
 }
-
-
-//		s = getSharedPreferences("preferences", MODE_PRIVATE);
-//		au = (CheckBoxPreference) findPreference("autoupdate");
-//		au.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {			
-//			public boolean onPreferenceChange(Preference arg0, Object arg1) {
-//				//Log.v("pulWifi","estamos en Preferences.onPreferenceChange() autoupdate");
-//				SharedPreferences.Editor e = s.edit();
-//				if(au.isChecked()){
-//					//Log.v("pulWifi","autoupdate isChecked() == true");
-//					au.setChecked(false);
-//					e.putBoolean("autoupdate",false);
-//				}else{
-//					//Log.v("pulWifi","autoupdate isChecked() == false");
-//					au.setChecked(true);
-//					e.putBoolean("autoupdate",true);
-//				}
-//				//Log.v("pulWifi","hacemos commit() y nos las piramos del Preferences.onPreferenceChange() autoupdate");
-//				e.commit();
-//				return false;
-//			}
-//		});
-//	
-//		vF = (CheckBoxPreference) findPreference("vibrateFOUND");
-//		vF.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-//		
-//			public boolean onPreferenceChange(Preference arg0, Object arg1) {
-//				//Log.v("pulWifi","estamos en Preferences.onPreferenceChange() vibrateFOUND");
-//				SharedPreferences.Editor e = s.edit();
-//				if(vF.isChecked()){
-//					//Log.v("pulWifi","vibrateFOUND isChecked() == true");
-//					vF.setChecked(false);
-//					e.putBoolean("vibrateFOUND",false);
-//				}else{
-//					//Log.v("pulWifi","vibrateFOUND isChecked() == false");
-//					vF.setChecked(true);
-//					e.putBoolean("vibrateFOUND",true);
-//				}
-//				//Log.v("pulWifi","hacemos commit() y nos las piramos del Preferences.onPreferenceChange() vibrateFOUND");
-//				e.commit();
-//				return false;
-//			}
-//		});
-//		
-//		vU = (CheckBoxPreference) findPreference("vibrateUPDATE");
-//		vU.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-//		
-//			public boolean onPreferenceChange(Preference arg0, Object arg1) {
-//				//Log.v("pulWifi","estamos en Preferences.onPreferenceChange() vibrateUPDATE");
-//				SharedPreferences.Editor e = s.edit();
-//				if(vU.isChecked()){
-//					//Log.v("pulWifi","vibrateUPDATE isChecked() == true");
-//					vU.setChecked(false);
-//					e.putBoolean("vibrateUPDATE",false);
-//				}else{
-//					//Log.v("pulWifi","vibrateUPDATE isChecked() == false");
-//					vU.setChecked(true);
-//					e.putBoolean("vibrateUPDATE",true);
-//				}
-//				//Log.v("pulWifi","hacemos commit() y nos las piramos del Preferences.onPreferenceChange() vibateUPDATE");
-//				e.commit();
-//				return false;
-//			}
-//		});
-//		
-//		uI = (ListPreference) findPreference("updateInterval");
-//		uI.setOnPreferenceChangeListener(new OnPreferenceChangeListener(){			
-//			public boolean onPreferenceChange(Preference arg0, Object arg1) {
-//				Log.e("pulWifi","estamos en onPreferenceChange() updateInterval");
-//				SharedPreferences.Editor e= s.edit();
-//				String update = uI.getValue();
-//				Log.e("pulWifi",update);
-//				e.putString("updateInterval",update);
-//				e.commit();
-//				return false;
-//			}
-//		});
-//	}
-//}
