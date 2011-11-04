d53bad56149552b90182af3990ce8121ce020898
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2011-11-04 19:15:09 +0100

    Refactor WirelessNetwork.java

--- src/es/pulimento/wifi/WirelessNetwork.java
+++ src/es/pulimento/wifi/WirelessNetwork.java
@@ -3,128 +3,111 @@ package es.pulimento.wifi;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class WirelessNetwork implements Parcelable,Comparable<WirelessNetwork> {
-	
-		//Atributos
-		private String mEssid, mBssid, capabilities;
-		private boolean crackeable;
-		public String clave;
-		int signal;		
-		
-		//public static WirelessNetwork current;
-		
-		//Constructores
-		public WirelessNetwork(String ESSID, String BSSID, boolean isCrackeable, int signal, String capabilities) {
-			mEssid = ESSID;
-			mBssid = BSSID;
-			crackeable = isCrackeable;
-			clave = null;
-			this.signal = signal;
-			this.capabilities = capabilities;
-			//current = this;
-		}
-		
-		/*public WirelessNetwork(String ESSID, String BSSID) {
-			mEssid = ESSID;
-			mBssid = BSSID;
-			crackeable = isCrackeable(this);
-			clave = null;
-			//current = this;
-		}*/
+public class WirelessNetwork implements Parcelable, Comparable<WirelessNetwork> {
+	// TODO: Be able of processing capabilities.
+	// TODO: There should be a crack function that updates password variable.
 
-		public WirelessNetwork(Parcel in) {
-			readFromParcel(in);
-			//current = this;
-		}
+	private String mEssid, mBssid, mCapabilities, mPassword;
+	private boolean mCrackeable;
+	private int mSignal;
 
-		public String getEssid() {
-			return mEssid;
-		}
-		
-		public int getSignal(){
-			return signal;
-		}
+	public WirelessNetwork(String ESSID, String BSSID, boolean crackeable, int signal, String capabilities) {
+		mEssid = ESSID;
+		mBssid = BSSID;
+		mCrackeable = crackeable;
+		mPassword = null;
+		mSignal = signal;
+		mCapabilities = capabilities;
+	}
 
-		public String getBssid() {
-			return mBssid;
-		}
-		
-		public String getClave() {
-			return clave;
-		}
-	
-		public boolean getCrackeable(){
-			return crackeable;
-		}
-		
-		public String getCapabilities(){
-			return capabilities;
-		}
-		
+	public WirelessNetwork(Parcel in) {
+		// We just need to read back each field in the order that it was written to the parcel.
+		mEssid = in.readString();
+		mBssid = in.readString();
+		boolean c[] = new boolean[1];
+		in.readBooleanArray(c);
+		mCrackeable = c[0];
+		mPassword = in.readString();
+		mSignal = in.readInt();
+		mCapabilities = in.readString();
+	}
 
-		public void setmEssid(String mEssid) {
-			this.mEssid = mEssid;
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// We just need to write each field into the parcel.
+		// When we read from parcel, they will come back in the same order.
+		dest.writeString(mEssid);
+		dest.writeString(mBssid);
+		dest.writeBooleanArray(new boolean[]{ mCrackeable });
+		dest.writeString(mPassword);
+		dest.writeInt(mSignal);
+		dest.writeString(mCapabilities);
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	public static final Parcelable.Creator<WirelessNetwork> CREATOR = new Parcelable.Creator<WirelessNetwork>() {
+		public WirelessNetwork createFromParcel(Parcel in) {
+			return new WirelessNetwork(in);
 		}
 
-		public void setmBssid(String mBssid) {
-			this.mBssid = mBssid;
+		public WirelessNetwork[] newArray(int size) {
+			return new WirelessNetwork[size];
 		}
+	};
 
-		public void setCrackeable(boolean crackeable) {
-			this.crackeable = crackeable;
-		}
+	public String getEssid() {
+		return mEssid;
+	}
 
-		public void setClave(String clave) {
-			this.clave = clave;
-		}
+	public void setEssid(String ESSID) {
+		mEssid = ESSID;
+	}
 
-		public int describeContents() {
-			return 0;
-		}
-		
-		
-		//@Override
-		public void writeToParcel(Parcel dest, int flags) {
-			// We just need to write each field into the
-			// parcel. When we read from parcel, they
-			// will come back in the same order
-			dest.writeString(mEssid);
-			dest.writeString(mBssid);
-			dest.writeBooleanArray(new boolean[]{ crackeable });
-		}
+	public int getSignal() {
+		return mSignal;
+	}
 
-		//@Override
-		private void readFromParcel(Parcel in) {
-			// We just need to read back each
-			// field in the order that it was
-			// written to the parcel
-			mEssid = in.readString();
-			mBssid = in.readString();
-			boolean c[] = new boolean[1];
-			in.readBooleanArray(c);
-			crackeable = c[0];
-		}
+	public String getBssid() {
+		return mBssid;
+	}
 
-		public static final Parcelable.Creator<WirelessNetwork> CREATOR =
-	    	new Parcelable.Creator<WirelessNetwork>() {
-	            public WirelessNetwork createFromParcel(Parcel in) {
-	                return new WirelessNetwork(in);
-	            }
-	 
-	            public WirelessNetwork[] newArray(int size) {
-	                return new WirelessNetwork[size];
-	            }
-	        };
+	public void setBssid(String BSSID) {
+		mBssid = BSSID;
+	}
 
-		public int compareTo(WirelessNetwork w0) {
-			// TODO Auto-generated method stub
-			if(this.getCrackeable()){
-				if(w0.getCrackeable()){
-					return w0.getSignal() - this.getSignal();
-				}
-				return -1;
-			}			
-			else if(w0.getCrackeable()) return 1;
-			return 0;
+	public String getClave() {
+		return mPassword;
+	}
+
+	public void setClave(String clave) {
+		mPassword = clave;
+	}
+
+	public boolean getCrackeable() {
+		return mCrackeable;
+	}
+
+	public void setCrackeable(boolean crackeable) {
+		mCrackeable = crackeable;
+	}
+
+	public String getCapabilities() {
+		return mCapabilities;
+	}
+
+	public int compareTo(WirelessNetwork w0) {
+		if(this.getCrackeable()) {
+			if(w0.getCrackeable()) {
+				return w0.getSignal() - this.getSignal();
+			}
+			return -1;
 		}
+		else if(w0.getCrackeable())
+			return 1;
+		return 0;
+	}
 }
