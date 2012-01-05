d27e5656cb175e022eed635bb4610aeecaf43684
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2012-01-05 22:32:25 +0100

    Finish updating system. Needs to be tested.

--- src/es/pulimento/wifi/ui/Updater.java
+++ src/es/pulimento/wifi/ui/Updater.java
@@ -1,11 +1,7 @@
 package es.pulimento.wifi.ui;
 
 import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -17,8 +13,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -32,9 +26,8 @@ public class Updater extends Activity implements OnClickListener {
 	private TextView mLatestVersion;
 	private Button updateButton;
 
-	private final String VERSION_URL = "https://raw.github.com/pulWifi/pulWifi/master/version_latest";
+	private final String VERSION_URL = "https://raw.github.com/antoniovazquezblanco/pulWifi/master/version_latest";
 	private final String APK_URL = "https://github.com/downloads/pulWifi/pulWifi/pulWifi_%s_signed.apk";
-	private final String APK_NAME = "pulWifi.apk";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -58,29 +51,12 @@ public class Updater extends Activity implements OnClickListener {
 			new GetLatestVersion().execute();
 			break;
 		case R.id.layout_updater_update:
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(APK_URL, mLatestVersion.getText()))));
 			break;
 		}
 	}
 
-	public void Update(String apkurl) throws MalformedURLException, IOException {
-		HttpURLConnection c = (HttpURLConnection) new URL(apkurl).openConnection();
-		c.setRequestMethod("GET");
-		c.setDoOutput(true);
-		c.connect();
-		File file = new File(Environment.getExternalStorageDirectory()+ "/downloads/");
-		file.mkdirs();
-		FileOutputStream fos = new FileOutputStream(new File(file, APK_NAME));
-		InputStream is = c.getInputStream();
-		byte[] buffer = new byte[1024];
-		int len = 0;
-		while ((len = is.read(buffer)) != -1)
-			fos.write(buffer, 0, len);
-		fos.close();
-		is.close();
-		startActivity(new Intent(Intent.ACTION_VIEW).setDataAndType(Uri.fromFile(new File(Environment.getExternalStorageDirectory()+ "/downloads/app.apk")), "application/vnd.android.package-archive"));
-	}
-
-	public class GetLatestVersion extends AsyncTask<Void, Void, Integer> {
+	public class GetLatestVersion extends AsyncTask<Void, Void, String> {
 
 		private ProgressDialog dialog;
 
@@ -90,35 +66,35 @@ public class Updater extends Activity implements OnClickListener {
 		}
 
 		@Override
-		protected Integer doInBackground(Void... params) {
+		protected String doInBackground(Void... params) {
 			try {
 				byte [] versionData = new byte[6];
 				(new DataInputStream((new URL(VERSION_URL)).openConnection().getInputStream())).read(versionData);
-				Log.v("PULW", String.valueOf(versionData));
+				return new String(versionData).trim();
 			} catch (MalformedURLException e) {
 				// TODO: This should not be fired. Should be reported.
-				return -1;
+				return "ERR";
 			} catch (UnknownHostException e) {
 				// Network error.
-				return -1;
+				return "ERR";
 			} catch (IOException e) {
 				// TODO: Check what causes this.
-				return -1;
+				return "ERR";
 			}
-			return -1;
 		}
 
 		@Override
-		public void onPostExecute(Integer res) {
-			if(res == -1)
+		public void onPostExecute(String res) {
+			if(res == "ERR")
 			{
 				Toast.makeText(mContext, R.string.updater_error, Toast.LENGTH_LONG).show();
 				updateButton.setVisibility(View.GONE);
 			}
 			else
 			{
-				mLatestVersion.setText(String.valueOf(res));
-				updateButton.setVisibility(View.VISIBLE);
+				mLatestVersion.setText(res);
+				if(res != mContext.getString(R.string.app_version))
+					updateButton.setVisibility(View.VISIBLE);
 			}
 			dialog.dismiss();
 			return;
