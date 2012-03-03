1c359b6d6eefb667e68c99c46df8ccfe1f5b3c23
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2012-03-03 21:47:01 +0100

    Fix warning

--- src/es/pulimento/wifi/core/algorithms/AlgorithmList.java
+++ src/es/pulimento/wifi/core/algorithms/AlgorithmList.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class AlgorithmList extends ArrayList<CrackAlgorithm> {
 
+	private static final long serialVersionUID = 9078810203314526774L;
+
 	int working_algo = -1;
 
 	public boolean isCrackeable() {
