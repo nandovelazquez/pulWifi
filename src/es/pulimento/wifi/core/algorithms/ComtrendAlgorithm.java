19d95f3fc91671cdbc4b5320b58ae44418a88a31
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2012-03-21 21:51:54 +0100

    Update Comtrend algorithm

--- src/es/pulimento/wifi/core/algorithms/ComtrendAlgorithm.java
+++ src/es/pulimento/wifi/core/algorithms/ComtrendAlgorithm.java
@@ -14,21 +14,17 @@ import es.pulimento.wifi.core.WirelessNetwork.WirelessEncryption;
  * This routers use an WPA encryption.
  * The corresponding mac addresses are:
  * 64:68:0C:XX:XX:XX (WLAN_XXXX & JAZZTEL_XXXX)
- * 
+ * 00:1D:20:XX:XX:XX (WLAN_XXXX & JAZZTEL_XXXX)
+ * 00:1B:20:XX:XX:XX (WLAN_XXXX & JAZZTEL_XXXX)
+ * 00:23:F8:XX:XX:XX (WLAN_XXXX & JAZZTEL_XXXX)
+ * 00:1F:A4:XX:XX:XX (WLAN_XXXX & JAZZTEL_XXXX)
+ *
  * Unsuported/to be checked mac addresses:
  * 00:1A:2B:XX:XX:XX (266 keys...)
- * 00:1D:20:XX:XX:XX (I think it is supported...)
- * 00:1B:20:XX:XX:XX (I think it is supported...)
- * 00:23:F8:XX:XX:XX (See wpamagickey / I think it is supported...)
- * 00:1F:A4:XX:XX:XX (I thikn it is supported...)
  * 38:72:C0:XX:XX:XX
  * F4:3E:61:XX:XX:XX
  * 30:39:F2:XX:XX:XX
  * 00:19:15:XX:XX:XX (TECOM / Telephone numbers? Search...)
- * Information at:
- * Google search: andreagonzalez2k
- * Google search: wpamagickey
- * http://foro.seguridadwireless.net/aplicaciones-y-diccionarios-linux/wpamagickey-claves-por-defecto-wlan_xxxx-jazztel_xxxx-routers-comtrend-35300/
  */
 public class ComtrendAlgorithm extends CrackAlgorithm {
 
@@ -49,13 +45,45 @@ public class ComtrendAlgorithm extends CrackAlgorithm {
 		// ESSID: JAZZTEL_XXXX
 		// BSSID: 64:68:0C:XX:XX:XX
 		addPattern("JAZZTEL_([0-9a-fA-F]{4})", "(64:68:0C:[0-9A-Fa-f:]{8})");
+
+		// ESSID: WLAN_XXXX
+		// BSSID: 00:1D:20:XX:XX:XX
+		addPattern("WLAN_([0-9a-fA-F]{4})", "(00:1D:20:[0-9A-Fa-f:]{8})");
+
+		// ESSID: JAZZTEL_XXXX
+		// BSSID: 00:1D:20:XX:XX:XX
+		addPattern("JAZZTEL_([0-9a-fA-F]{4})", "(00:1D:20:[0-9A-Fa-f:]{8})");
+
+		// ESSID: WLAN_XXXX
+		// BSSID: 00:1B:20:XX:XX:XX
+		addPattern("WLAN_([0-9a-fA-F]{4})", "(00:1B:20:[0-9A-Fa-f:]{8})");
+
+		// ESSID: JAZZTEL_XXXX
+		// BSSID: 00:1B:20:XX:XX:XX
+		addPattern("JAZZTEL_([0-9a-fA-F]{4})", "(00:1B:20:[0-9A-Fa-f:]{8})");
+
+		// ESSID: WLAN_XXXX
+		// BSSID: 00:23:F8:XX:XX:XX
+		addPattern("WLAN_([0-9a-fA-F]{4})", "(00:23:F8:[0-9A-Fa-f:]{8})");
+
+		// ESSID: JAZZTEL_XXXX
+		// BSSID: 00:23:F8:XX:XX:XX
+		addPattern("JAZZTEL_([0-9a-fA-F]{4})", "(00:23:F8:[0-9A-Fa-f:]{8})");
+
+		// ESSID: WLAN_XXXX
+		// BSSID: 00:1F:A4:XX:XX:XX
+		addPattern("WLAN_([0-9a-fA-F]{4})", "(00:1F:A4:[0-9A-Fa-f:]{8})");
+
+		// ESSID: JAZZTEL_XXXX
+		// BSSID: 00:1F:A4:XX:XX:XX
+		addPattern("JAZZTEL_([0-9a-fA-F]{4})", "(00:1F:A4:[0-9A-Fa-f:]{8})");
 	}
 
 	@Override
 	protected String crackAlgorithm(String essid_data, String bssid_data) {
 		bssid_data = bssid_data.replace(":", "").toUpperCase();
 		essid_data = essid_data.toUpperCase();
-   		return MD5Hash("bcgbghgg"+bssid_data.substring(0,8)+essid_data.substring(essid_data.length()-4,essid_data.length())+bssid_data);
+		return MD5Hash("bcgbghgg"+bssid_data.substring(0,8) + essid_data + bssid_data);
 	}
 
 	private static String MD5Hash(String input) {
