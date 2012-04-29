93896cc4aaf898acaa9e071e760a63acc51f2390
Javi Pulido <pulimento@gmail.com>
2012-04-29 17:12:29 +0200

    Fixed "Supported networks dialog not showing properly on some devices", now is html-based

--- src/es/pulimento/wifi/ui/dialogs/SupportedNetworksDialog.java
+++ src/es/pulimento/wifi/ui/dialogs/SupportedNetworksDialog.java
@@ -22,19 +22,24 @@ package es.pulimento.wifi.ui.dialogs;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.webkit.WebView;
 import es.pulimento.wifi.R;
 
 public class SupportedNetworksDialog extends Dialog {
 
+	private Context mContext;
+
 	public SupportedNetworksDialog(Context context) {
 		super(context);
+		mContext = context;
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		this.setContentView(R.layout.dialog_supportednetworks);
+		this.setContentView(R.layout.dialog_generic);
 		this.setTitle(R.string.dialog_supportednetworks_title);
+
+		((WebView) findViewById(R.id.dialog_generic_webview)).loadUrl(mContext.getString(R.string.dialog_supportednetworks_file_url));
 	}
 }
