6c665e4244773b6666407316dc04f7bd4638f115
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2012-05-24 19:16:32 +0200

    Delete unuseful imports.

--- src/es/pulimento/wifi/ui/utils/ExceptionHandler.java
+++ src/es/pulimento/wifi/ui/utils/ExceptionHandler.java
@@ -4,8 +4,6 @@ import java.lang.Thread.UncaughtExceptionHandler;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
