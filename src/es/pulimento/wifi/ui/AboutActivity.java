e99a3687f2fb71d7070b0c1df1508157250ae758
Javi Pulido <pulimento@gmail.com>
2012-07-30 00:44:02 +0200

    Fix an import

--- src/es/pulimento/wifi/ui/AboutActivity.java
+++ src/es/pulimento/wifi/ui/AboutActivity.java
@@ -3,7 +3,6 @@ package es.pulimento.wifi.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
